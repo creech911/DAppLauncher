f60070db92dd0b9adf5d1f5f87e6bd6adee6fe08
creech911 <163350628+creech911@users.noreply.github.com>
2024-08-11 13:08:19 +0200

    Init

---
+++ index.js
@@ -0,0 +1,20 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Axios from "axios";
+import HomePage from "./components/HomePage";
+import AboutPage from "./components/AboutPage";
+import NotFoundPage from "./components/NotFoundPage";
+Axios.defaults.baseURL = process.env.REACT_APP_API_BASE_URL;
+const App = () => (
+  <Router>
+    <div>
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route path="/about" component={AboutPage} />
+        <Route component={NotFoundPage} />
+      </Switch>
+    </div>
+  </Router>
+);
+ReactDOM.render(<App />, document.getElementById('root'));
