cb352146e8610f8c29962afb7f141ca4a1c62db3
creech911 <163350628+creech911@users.noreply.github.com>
2024-08-13 16:29:56 +0200

    clean code

--- index.js
+++ index.js
@@ -5,16 +5,19 @@ import Axios from "axios";
 import HomePage from "./components/HomePage";
 import AboutPage from "./components/AboutPage";
 import NotFoundPage from "./components/NotFoundPage";
+
 Axios.defaults.baseURL = process.env.REACT_APP_API_BASE_URL;
+
 const App = () => (
   <Router>
-    <div>
-      <Switch>
-        <Route exact path="/" component={HomePage} />
-        <Route path="/about" component={AboutPage} />
-        <Route component={NotFoundPage} />
-      </Switch>
-    </div>
+    <Switch>
+      <Route exact path="/" component={HomePage} />
+      <Route path="/about" component={AboutPage} />
+      <Route component={NotFoundPage} />
+    </Switch>
   </Router>
 );
+
 ReactDOM.render(<App />, document.getElementById('root'));
+
+export default App;
